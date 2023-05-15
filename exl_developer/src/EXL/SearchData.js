945d77caf834c2801fd8608ad48dfa8313f26d95
DIKSHIT AHAR <ahardikshit@hotmail.com>
2023-05-15 19:20:54 +0530

    react vode deployment

---
+++ exl_developer/src/EXL/SearchData.js
@@ -0,0 +1,81 @@
+import React, { useMemo } from 'react';
+import { useTable, useSortBy, useGlobalFilter } from 'react-table';
+
+const SearchData = ({ data }) => {
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Data',
+        accessor: 'data',
+      },
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useGlobalFilter,
+    useSortBy
+  );
+
+  const { globalFilter } = state;
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={globalFilter || ''}
+        onChange={(e) => setGlobalFilter(e.target.value)}
+        placeholder="Search..."
+      />
+
+      <table {...getTableProps()} className="table">
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  className={
+                    column.isSorted
+                      ? column.isSortedDesc
+                        ? 'sorted-desc'
+                        : 'sorted-asc'
+                      : ''
+                  }
+                >
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default SearchData;
