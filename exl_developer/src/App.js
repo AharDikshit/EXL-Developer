62808dc28e9112634b78a837610654986ff982a4
DIKSHIT AHAR <ahardikshit@hotmail.com>
2023-05-15 22:52:08 +0530

    staged changes

--- exl_developer/src/App.js
+++ exl_developer/src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import SearchData from './EXL/SearchData';
-import axios from 'axios';
+import TableData from './EXL/TableData';
 
 function App() {
-  const data= axios.fetch("https://dummyjson.com/products")
   return (
     <div className="App">
       <header className="App-header">
         <h1>Table data</h1>
-        <SearchData data={data} />
+        <TableData />
       </header>
     </div>
   );
